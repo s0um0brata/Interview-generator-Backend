929ca4eb2ffa67928f82aa764fb5d3ee3d0b51cf
Soumobrata Chanda <chandadeep8@gmail.com>
2026-05-20 21:53:32 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,29 @@
+// const mongoose = require("mongoose");
+
+// async function connectToDB() {
+//     try {
+//         await mongoose.connect(process.env.MONGO_URI);
+
+//         console.log("Connected to Database")
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// module.exports = connectToDB;
+
+
 const mongoose = require("mongoose");
 
-async function connectToDB() {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-
-        console.log("Connected to Database")
-    } catch (error) {
-        console.log(error);
-    }
+function connectToDB() {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("MongoDB connected successfully");
+    })
+    .catch((err) => {
+      console.log("MongoDB connection error:", err);
+    });
 }
 
 module.exports = connectToDB;
